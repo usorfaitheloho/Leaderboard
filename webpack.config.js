b6fb2ee3a23da448393f143905cfb6f62bd42301
usorfaitheloho <usorfaitheloho@gmail.com>
2022-03-09 10:22:18 -0800

    Fix errors

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode:'production',
+  mode:'development',
   entry: {
     index: './src/index.js',
   },
@@ -24,6 +24,14 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
   },

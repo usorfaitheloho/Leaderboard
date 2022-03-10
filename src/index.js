440f5238139cbb5efe324eea38689077303ba05d
usorfaitheloho <usorfaitheloho@gmail.com>
2022-03-09 16:51:24 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './style.css';
+import {
+  addScores,getScore,userInput,scoreInput,
+} from './modules/lead-api.js';
+
+const submitBtn = document.querySelector('#submit-btn');
+const failAlert = document.querySelector('.fail-alert');
+const refreshBtn = document.querySelector('#refresh-btn');
+
+window.addEventListener('pageshow',()=>{
+  getScore();
+});
+
+refreshBtn.addEventListener('click', () =>{
+  getScore();
+});
+
+submitBtn.addEventListener('click',()=>{
+  if(scoreInput.value === '' || userInput.value === ''){
+    scoreInput.value = '';
+    failAlert.innerHTML = 'Submission failed. Try again.';
+  }else{
+    addScores();
+  }
+});
+
+scoreInput.addEventListener('click',() =>{
+  failAlert.innerHTML = '';
+});

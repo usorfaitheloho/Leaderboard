bedd1dd14a01519dd224c18f3e14b1449bc0c30c
usorfaitheloho <usorfaitheloho@gmail.com>
2022-03-10 11:14:48 -0800

    FIx

--- src/modules/score.js
+++ src/modules/score.js
@@ -3,8 +3,8 @@ import { getScore } from './lead-api.js';
 const scoreBoard = document.querySelector('#score-board');
 
 const renderScore = async (data) => {
- let scoreHTML = '';
- data.forEach(({ user, score }) => {
+  let scoreHTML = '';
+  data.forEach(({ user, score }) => {
     scoreHTML += `<li>
       <p class="name">${user}</p>
       <p class="score">${score}</p>
@@ -13,9 +13,9 @@ const renderScore = async (data) => {
   });
   scoreBoard.innerHTML = scoreHTML;
 };
- const refreshScore = async () => {
- const data = await getScore();
- data.sort((player1, player2) => player2.score - player1.score);
+const refreshScore = async () => {
+  const data = await getScore();
+  data.sort((player1, player2) => player2.score - player1.score);
   renderScore(data);
 };
 export default refreshScore();
